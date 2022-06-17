25b08a7b9e81e2f00ce9d145b228f4388e52ec65
Neeraj Pratap Hazarika <72177954+NeerajHazarika@users.noreply.github.com>
2022-06-18 04:14:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,43 +3,45 @@ import requests
 from bs4 import BeautifulSoup
 import redis
 from rq import Queue
-
-import time
+import logging
 
 app = Flask(__name__)
-app.app_context().push()
 
 r = redis.Redis()
 q = Queue(connection=r)
 
+# logs will be stored at logfile.log from debug level to critical level
+logging.basicConfig(filename='logfile.log', level=logging.DEBUG)
+
 
 def background_task(tickerInput):
 
-    print("Task running")
-
-    resp = requests.get('https://api.nasdaq.com/api/news/topic/articlebysymbol?q={tickerInput}|stocks&offset=0&limit=8&fallback=false', headers={
+    resp = requests.get(f"https://api.nasdaq.com/api/news/topic/articlebysymbol?q={tickerInput}|stocks&offset=0&limit=8&fallback=false", headers={
         "User-agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'})
-    data = resp.json()
-    articles = []
-    for i in data['data']['rows']:
-        print(i['title'])
-        articles.append(i['title'])
 
-    print("Task complete")
+    if resp.status_code == 200:
 
-    r.sadd(tickerInput, *articles)
+        data = resp.json()
+
+        articles = []
+
+        for i in data['data']['rows']:
+            articles.append(i['title'])
+
+        r.sadd(tickerInput, *articles)
+
+    else:
+        app.logger.error("nasdaq server issue, api isnt giving any response")
 
 
 @app.route("/fetchNews")
 def add_task():
-
     if request.args.get("tickerInput"):
-
         job = q.enqueue(background_task, request.args.get("tickerInput"))
-        print(type(job))
-        return f"{job}"
-
-    return "No value for tickerInput"
+        q_len = len(q)
+        return f"Task {job.id} added to queue at {job.enqueued_at}. {q_len} tasks in the queue"
+    else:
+        return "No value for tickerInput"
 
 
 if __name__ == "__main__":
